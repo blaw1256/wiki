15e5f498b1bf7b37ded41b1c5e133580c349f3bb
blaw1256 <brenbrenas@gmail.com>
2026-04-28 14:42:21 +0100

    testing

--- test.cmd
+++ test.cmd
@@ -2,5 +2,5 @@ cd C:\Users\Brendan\OneDrive - University College Cork\Desktop\dumb shit\Wiki fo
 git config --global user.email "brenbrenas@gmail.com"
 git config --global user.name "blaw1256"
 git add .
-git commit -m "gitignore changes"
+git commit -m "testing"
 git push origin main
