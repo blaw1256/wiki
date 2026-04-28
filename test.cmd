5ad0a78623907b4d9c5fb99dc0cac2fae80c4d85
blaw1256 <brenbrenas@gmail.com>
2026-04-28 14:43:40 +0100

    testing

--- test.cmd
+++
@@ -1,6 +0,0 @@
-cd C:\Users\Brendan\OneDrive - University College Cork\Desktop\dumb shit\Wiki for pf
-git config --global user.email "brenbrenas@gmail.com"
-git config --global user.name "blaw1256"
-git add .
-git commit -m "testing"
-git push origin main
