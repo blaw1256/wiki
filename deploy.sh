9eaea96c5e2c2975249f67ef04818c0748cba6f1
blaw1256 <brenbrenas@gmail.com>
2026-04-28 14:27:51 +0100

    troson changes

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd C:\Users\Brendan\OneDrive - University College Cork\Desktop\dumb shit\Wiki for pf
+git config --global user.email "brenbrenas@gmail.com"
+git config --global user.name "blaw1256"
+git add .
+git commit -m "troson changes"
+git push origin main
